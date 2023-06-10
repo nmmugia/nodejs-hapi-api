1b9718d8fa8d559850955cde51ec08de7f4e1237
nmmugia <seemugia1@gmail.com>
2023-06-11 05:52:26 +0700

    repo: add functions to fetch products from woo
    Signed-off-by: nmmugia <seemugia1@gmail.com>

---
+++ src/repositories/woo/product.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+async function fetchProducts(page: number): Promise<any> {
+  const url = `https://codetesting.jubelio.store/wp-json/wc/v3/products?per_page=50&page=${page}&orderby=date`;
+  const username = process.env.WOO_CLIENT_KEY;
+  const password = process.env.WOO_CLIENT_SECRET;
+  const authHeader = `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`;
+  try {
+    const response = await axios.get(url, {
+        headers: {
+          Authorization: authHeader,
+        },
+      });
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching products from page ${page}:`, error);
+    return [];
+  }
+}
+
+export async function fetchAllProducts(): Promise<any[]> {
+  let page = 1;
+  const allProducts: any[] = [];
+
+  // Fetch 2 at one iteration (2 workers concurrently)
+  while (true) {
+    const requests = [fetchProducts(page), fetchProducts(page + 1)];
+    const results = await Promise.allSettled(requests);
+
+    for (const result of results) {
+      if (result.status === 'fulfilled') {
+        const products = result.value;
+
+        if (products.length === 0) {
+          return allProducts; // Exit the loop when there are no more products
+        }
+
+        allProducts.push(...products);
+      }
+    }
+
+    page += 2; // Increment the page by 2 for the next set of requests
+  }
+}
